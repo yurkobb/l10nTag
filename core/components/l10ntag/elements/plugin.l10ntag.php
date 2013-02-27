9e173aa11166efaaa4a4f1c490ee2ccba59cd456
Constantine Kozlov <tempos@parmasse.andhor>
2013-02-27 23:06:40 +0200

    Bug fixes, cleanup and small optimization on getStartTag

--- core/components/l10ntag/elements/plugin.l10ntag.php
+++ core/components/l10ntag/elements/plugin.l10ntag.php
@@ -1,13 +1,13 @@
 <?php
-$babelparser = $modx->getService('babelparser', 'BabelParser', $modx->getOption('core_path') . 'components/l10ntag/model/babelparser/');
+ $babelparser = $modx->getService('babelparser', 'BabelParser', $modx->getOption('core_path') . 'components/l10ntag/model/babelparser/');
 
 if (!($babelparser instanceof BabelParser)) return;
 
 //$output = $babelparser->parseString($modx->documentOutput);
-$output = &$modx->documentOutput;
-$result = $babelparser->parseString($output);
+$output = &$modx->resource->_output;
+$output = $babelparser->parseString($output);
 
 //$modx->log(modX::LOG_LEVEL_DEBUG, '[l10nTag] input = ' . $content);
 //$modx->log(modX::LOG_LEVEL_DEBUG, '[l10nTag] output = ' . $output);
 
-$modx->documentOutput = $result;
+//$modx->documentOutput = $result;
