1e80f913a631eebc1a2cc206f6d92820f8a2c376
MornieOhtar <tempor.demonius@gmail.com>
2013-02-27 23:59:57 +0200

    Fixed issue, when content gone on short TV's; tags and culture keys now in class vars.

--- core/components/l10ntag/model/babelparser/babelparser.class.php
+++ core/components/l10ntag/model/babelparser/babelparser.class.php
@@ -1,21 +1,22 @@
 <?php
 
 class BabelParser {
+	public $TAGSTART = "[@";
+	public $TAGEND = "]";
+	public $cultureKeyLength = 2;
 	public function getTagStart ($input, $offset) {
 		return strpos($input, $this->TAGSTART, $offset);
 	}
 
 	public function getTagEnd ($input, $offset) {
 		$tagEnd = strpos($input, $this->TAGEND, $offset) + $this->tagEndLen;
-		while ($tagEnd && $input[$tagEnd+1] == ']') {
+		while ($tagEnd && $input[$tagEnd+1] == $this->TAGEND) {
 			$tagEnd++;
 		}
 		return $tagEnd;
 	}
 
 	public function __construct (&$modx, $options) {
-		$this->TAGSTART="[@";
-		$this->TAGEND="]";
 		$this->xpdo = $modx;
 		$this->tagStartLen = strlen($this->TAGSTART);
 		$this->tagEndLen = strlen($this->TAGEND);
@@ -30,20 +31,23 @@ class BabelParser {
 		while($tagStart!=false){
 			$tagEnd=$this->getTagEnd($var,$tagStart);
 			if(!$tagEnd){
-				$output.=substr($var,$tagStart);
+				$var=substr($var,$tagStart);
+				break;
 			}
 			$tag = substr($var,$tagStart,$tagEnd - $tagStart);
 			$newTag = $this->parseTag($tag);
 			$var = str_replace($tag, $newTag , $var);
-			$tagStart = $this->getTagStart($var,$tagEnd + 1);
+			$tagStart = $this->getTagStart($var,$tagEnd-(strlen($tag)-strlen($newTag)));
 		}
 		return $var;
 	}
 	public function parseTag($tag){
-		$tagCulture = substr($tag,2,2);
-		if($tagCulture == $this->cultureKey){
-//		if("ru" == "ru"){
+		$tagCulture = substr($tag,$this->tagStartLen,$this->cultureKeyLength);
+		if(substr($tag,0,$this->tagStartLen)==$this->TAGSTART && $tagCulture == $this->cultureKey){
 			return substr($tag,4,strlen($tag)-$this->tagStartLen - $this->tagEndLen-2);
+		}
+		else if(substr($tag,0,$this->tagStartLen)!=$this->TAGSTART){
+			return $tag;
 		}
 		else{
 			return "";
